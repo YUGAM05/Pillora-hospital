a3a6c090757f95e3528a112d4642b52fe0d89037
YUGAM05 <24002170210108@mail.ljku.edu.in>
2026-06-05 00:07:27 +0530

    fix(blog): render blog content as HTML using dangerouslySetInnerHTML

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -185,9 +185,7 @@ export default function BlogPostDetail() {
                         {blog.description}
                     </p>
 
-                    <div className="whitespace-pre-wrap text-gray-800 leading-loose text-lg font-medium space-y-6">
-                        {blog.content}
-                    </div>
+                    <div className="whitespace-pre-wrap text-gray-800 leading-loose text-lg font-medium space-y-6" dangerouslySetInnerHTML={{ __html: blog.content }} />
                 </motion.div>
 
                 {/* 5. Footer Navigation */}
